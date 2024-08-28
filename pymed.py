e45d8a79eeae935d71fd6f7acb0db1d1d438660f
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-08-28 15:49:31 +0530

    update

--- pymed.py
+++ pymed.py
@@ -4,7 +4,7 @@ from pymed import PubMed #PubMed library data
 pubmed = PubMed(tool="MyTool", email="#FILL MY EMAIL HERE")
 
 # Perform a query
-results = pubmed.query("nutrition", max_results=100)
+results = pubmed.query("prebiotic", max_results=100)
 
 # Process the results
 for article in results:
