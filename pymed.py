955ab00623b2bf270ba27a7a681e95322a96ffb7
Jadefangg <145111413+Jadefangg@users.noreply.github.com>
2024-08-28 12:42:34 +0530

    first commit pymed

---
+++ pymed.py
@@ -0,0 +1,12 @@
+from pymed import PubMed #PubMed library data
+
+# Initialize the PubMed object
+pubmed = PubMed(tool="MyTool", email="my@email.address")
+
+# Perform a query
+results = pubmed.query("nutrition", max_results=100)
+
+# Process the results
+for article in results:
+    print(article.title)
+    print(article.abstract)
